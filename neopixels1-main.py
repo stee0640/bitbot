856ffc33e562d6d9cf6ae24b3a5685cd92cae3cd
Steen Linden <steen@linden.dk>
2024-02-19 18:50:17 +0100

    Neopixels

---
+++ neopixels1-main.py
@@ -0,0 +1,18 @@
+from microbit import *
+import neopixel
+
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+cyan = (0,255,255)
+magenta = (255,0,255)
+yellow = (255,255,0)
+colors = [red, green, blue, cyan, magenta, yellow]
+
+np = neopixel.NeoPixel(pin13, 12)
+
+while True:
+    for color in colors:
+        np.fill(color)
+        np.show()
+        sleep(200)
